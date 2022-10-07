09a692490b91f4b0eb6e180e152ec7626f603d6a
Rafael Ortiz <rafael007@gmail.com>
2022-10-06 20:56:02 -0500

    Observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit(): void {
+    const obs$ = new Observable( observer => {
+
+      let i = -1;
+
+      const interval = setInterval( () => {
+        i++;
+        observer.next(i);
+
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('index i llegó al valor de 2');
+        }
+      }, 1000)
+
+    });
+
+    obs$.subscribe(
+      value => console.log('Subs:', value),
+      err => console.warn('Error: ', err),
+      () => console.info('Obs finished')
+    );
+
   }
 
 }

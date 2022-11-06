f8c796021fcb6469282a262e84c5ab81017d8e84
Rafael Ortiz <rafael007@gmail.com>
2022-11-06 11:03:47 -0500

    Convert usuarios response to response needed

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -14,7 +14,11 @@ export class Usuario {
     ) { }
 
     get imagenUrl() {
-        if (this.img) {
+        if (!this.img) {
+            return `${base_url}/upload/usuarios/no-image`;
+        } else if (this.img?.includes('https')) {
+            return  this.img;
+        } else if (this.img) {
             return `${base_url}/upload/usuarios/${this.img}`;
         } else {
             return `${base_url}/upload/usuarios/no-image`;
